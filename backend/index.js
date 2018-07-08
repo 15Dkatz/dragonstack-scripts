53d7884e0779a0a72d33e34a619d204f0d7aa7a1
David Katz <15Dkatz@shcp.edu>
2018-07-08 11:37:23 -0700

    Add Dragon Class with improvements

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,16 @@
-console.log("Hello Node");
+const Dragon = require('./dragon');
+
+const fooey = new Dragon({ birthdate: new Date(), nickname: 'fooey' });
+const baloo = new Dragon({ nickname: 'baloo', birthdate: new Date() });
+
+const mimar = new Dragon({});
+const gooby = new Dragon({});
+
+console.log('fooey', fooey);
+console.log('baloo', baloo);
+console.log('mimar', mimar);
+
+setTimeout(() => {
+  const gooby = new Dragon({});
+  console.log('gooby', gooby);
+}, 3000);
