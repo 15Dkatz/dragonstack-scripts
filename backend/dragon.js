c6e2d3e13febfb3e05b0f7d5dbe92b82020870c0
David Katz <15Dkatz@shcp.edu>
2018-07-09 21:28:56 -0700

    Support dragon traits

--- backend/dragon.js
+++ backend/dragon.js
@@ -1,14 +1,30 @@
+const TRAITS = require('./traits');
+
 const DEFAULT_PROPERTIES = {
   nickname: 'unnamed',
   get birthdate() {
     return new Date()
+  },
+  get randomTraits() {
+    const traits = {};
+
+    Object.keys(TRAITS).forEach(traitKey => {
+      const traitValues = TRAITS[traitKey];
+
+      traits[traitKey] = traitValues[
+        Math.floor(Math.random() * traitValues.length)
+      ]
+    });
+
+    return traits;
   }
 };
 
 class Dragon {
-  constructor({ birthdate, nickname }) {
+  constructor({ birthdate, nickname, traits } = {}) {
     this.birthdate = birthdate || DEFAULT_PROPERTIES.birthdate;
     this.nickname = nickname || DEFAULT_PROPERTIES.nickname;
+    this.traits = traits || DEFAULT_PROPERTIES.randomTraits;
   }
 }
 
--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,16 @@
 const Dragon = require('./dragon');
 
 const fooey = new Dragon({ birthdate: new Date(), nickname: 'fooey' });
-const baloo = new Dragon({ nickname: 'baloo', birthdate: new Date() });
-
-const mimar = new Dragon({});
-const gooby = new Dragon({});
+const baloo = new Dragon({
+  nickname: 'baloo',
+  birthdate: new Date(),
+  traits: {
+    backgroundColor: 'white',
+    pattern: 'patchy',
+    build: 'sporty',
+    size: 'large'
+  }
+});
 
 console.log('fooey', fooey);
 console.log('baloo', baloo);
-console.log('mimar', mimar);
-
-setTimeout(() => {
-  const gooby = new Dragon({});
-  console.log('gooby', gooby);
-}, 3000);
