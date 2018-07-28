8ccba7cb06cf0781e44e0fe1ad0cb54cc09dfd07
David Katz <15Dkatz@shcp.edu>
2018-07-27 22:25:31 -0700

    Introduce action creator

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,10 +7,12 @@ import './index.css';
 
 const DEFAULT_GENERATION = { generationId: '', expiration: '' };
 
-const generationReducer = (state, action) => {
-  console.log('generationReducer state action', state, action);
+const GENERATION_ACTION_TYPE = 'GENERATION_ACTION_TYPE';
 
-  if (action.type === 'GENERATION_ACTION_TYPE') {
+// const generationReducer = (state = { generation: DEFAULT_GENERATION }, action) => {
+// const generationReducer = (state) => {
+const generationReducer = (state, action) => {
+  if (action.type === GENERATION_ACTION_TYPE) {
     return { generation: action.generation };
   }
 
@@ -19,14 +21,54 @@ const generationReducer = (state, action) => {
 
 const store = createStore(generationReducer);
 
+store.subscribe(() => console.log('store state update', store.getState()));
+
 store.dispatch({ type: 'foo' });
 store.dispatch({
-  type: 'GENERATION_ACTION_TYPE',
+  type: GENERATION_ACTION_TYPE,
   generation: { generationId: 'goo', expiration: 'bar' }
 });
 
 console.log('store', store);
 console.log('store.getState()', store.getState());
+
+const generationActionCreator = payload => {
+//   // the object is the action
+  return {
+    type: GENERATION_ACTION_TYPE,
+    generation: payload
+  };
+}
+
+const zooAction = generationActionCreator(
+  { generationId: 'zoo', expiration: 'bar '}
+);
+
+store.dispatch(zooAction);
+
+fetch('http://localhost:3000/generation')
+  .then(response => response.json())
+  .then(json => store.dispatch(
+    generationActionCreator(json.generation))
+  );
+
+
+// setTimeout(() => {
+//   store.dispatch(
+//     generationActionCreator(zooAction)
+//   );
+// }, 2500);
+
+
+// // setTimeout(() => {
+//   fetch('http://localhost:3000/generation')
+//     .then(response => response.json())
+//     .then(json => {
+//       console.log('json', json);
+
+//       store.dispatch(generationActionCreator(json.generation))
+//     });
+// }, 5000);
 
 render(
   <div>
